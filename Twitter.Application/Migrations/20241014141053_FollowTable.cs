76961261595e9f8aae490887e1e659b52b4f2746
abanoub Saweris <abanoubphelopos12@gmail.com>
2024-10-14 18:04:19 +0300

    migrationfiles

---
+++ Twitter.Application/Migrations/20241014141053_FollowTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twitter.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class FollowTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Follow",
+                table: "Follow");
+
+            migrationBuilder.RenameTable(
+                name: "Follow",
+                newName: "Follows");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Follows",
+                table: "Follows",
+                columns: new[] { "FollowerId", "FolloweeId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Follows",
+                table: "Follows");
+
+            migrationBuilder.RenameTable(
+                name: "Follows",
+                newName: "Follow");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Follow",
+                table: "Follow",
+                columns: new[] { "FollowerId", "FolloweeId" });
+        }
+    }
+}
---
+++ Twitter.Application/Migrations/20241014143953_fixingTables.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twitter.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Follows_FolloweeId",
+                table: "Follows",
+                column: "FolloweeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows",
+                column: "FolloweeId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows",
+                column: "FollowerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FolloweeId",
+                table: "Follows");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Follows_AspNetUsers_FollowerId",
+                table: "Follows");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Follows_FolloweeId",
+                table: "Follows");
+        }
+    }
+}
